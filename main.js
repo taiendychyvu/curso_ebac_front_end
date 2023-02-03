4c68155545df0322804b199d386c0dd309a9bc73
taiendychyvu <86177862+taiendychyvu@users.noreply.github.com>
2023-02-03 16:14:49 -0300

    fix main.js

--- main.js
+++ main.js
@@ -6,12 +6,16 @@ $(document).ready(function(){
         const addTarefa = $(`<li>${novaTarefa}</li>`);
         $(addTarefa).appendTo(`ul`); //para adicionar tudo isso feito a cima em nosso ul do html
         $(`#nova-tarefa`).val(``); //para limpar o campo apos o click no botao
-    })
 
-    $(`li`).click(function(){
-        console.log(`clicou`)
-        $(this).css(`text-decoration`, `line-through`);
-    
+        $(`li`).click(function(){
+            $(this).css(`text-decoration`, `line-through`); 
+        })
     })
+//este codigo abaixo funciona fora do submit, o codigo li.click funciona apenas dentro do submit pois ele deve ser recarregado toda vez que uma li é adicionada no html
+/*     
+    $(document).on(`click`, `li`, function() {
+        $(this).css(`text-decoration`, `line-through`);
+    }) 
+*/
 }) 
 
